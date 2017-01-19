6b02c1de7f7f7723eb0fafa1302ec1f226c2c869
soyouda <soyouda@gmail.com>
2017-01-19 17:53:49 +0900

    local server test js

---
+++ myProject/20170115/server.js
@@ -0,0 +1,15 @@
+/**
+ * Created by jintae on 2017-01-19.
+ */
+var http = require('http');
+var fs = require('fs');
+
+var server = http.createServer(function(req, res){
+   fs.readFile('./index.html', 'utf8', function(err, data){
+       if(err) throw new Error('read Err');
+       res.end(data);
+   });
+});
+
+server.listen('8888');
+console.log('Listening on port 8888');
