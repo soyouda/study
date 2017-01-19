3fab6aeb41063f4de86f650cba828d7e3e6b5ca5
soyouda <soyouda@gmail.com>
2017-01-19 18:32:33 +0900

    local server test js

---
+++ myProject/20170115/default.js
@@ -0,0 +1,24 @@
+/**
+ * Created by jintae on 2017-01-19.
+ */
+
+var vendingMachine = {
+    power : false,
+    product : [{
+        name : '콜라',
+        price : 1000,
+        quantity : 0
+    }, {
+        name : '사이다',
+        price : 900,
+        quantity : 0
+    }],
+    returnButton : false,
+    cashBox : [{
+        bills : 10000,
+        coin : [{
+            coin500 : 500,
+            coin100 : 100
+        }]
+    }]
+};
